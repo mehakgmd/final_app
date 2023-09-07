b2480117cea56e73e0de9f2e01b8035e9baf104d
mehakgmd <115011143+mehakgmd@users.noreply.github.com>
2023-09-07 10:45:39 +0500

    solved some type issues

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,17 +1,27 @@
-import { NextResponse } from "next/server";
-import { createClient } from "next-sanity";
-import { SanityClient } from "sanity";
-import { client } from "@/lib/sanityClient";
+import { NextRequest, NextResponse } from "next/server";
+import { oneProductType } from "@/components/utils/ProductsDataArrayAndType";
 
 
-export  async function GET() {
-    try{
-       let response = await client.fetch(`*[_type == "products"]`);
-       console.log(response);
-       return NextResponse.json({response});
-    }catch(error) {
-        console.log((error as {message:string }).message )
-        return NextResponse.json({message: "Failed"})
-    }
+export  async function GET(request:NextRequest) {
+   const originalData: Array<oneProductType> = []; 
+   const url = request.nextUrl.searchParams;
+  
+   let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-07-03/data/query/production?query=*[_type == "products"]`) 
+    let dataFrom_API = await res.json()
+  originalData.push(...dataFrom_API.result)
+  
+  if(url.has("start") || url.has("end")){
+   if(originalData[Number(url.get("start"))]){
+    let productArray = originalData.slice(Number(url.get("start")), Number(url.get("end")))
+return NextResponse.json({productArray})
+}
+return NextResponse.json({productArray: "notfound"})
+}
+    return NextResponse.json({originalData})
+   
    
 };
+
+
+ //    let response = await client.fetch(`*[_type == "products"]`);
+    //   return NextResponse.json({response});
---
+++ app/products/page.tsx
@@ -0,0 +1,21 @@
+import BASE_PATH_FORAPI from '@/components/shared/BasePath';
+import AllProductsCompo from '@/components/views/AllProducts';
+
+async function fetchAllProductData(){
+  let  res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`)
+ if(!res.ok){
+  throw new Error("Failed to fetch")
+ }
+  return  res.json()
+};
+
+const Products = async () => {
+  const ProductData = await fetchAllProductData()
+  return (
+    <div>
+      <AllProductsCompo ProductData={ProductData}/>
+    </div>
+  )  
+}
+
+export default Products;
